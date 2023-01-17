ceb5b1501d4686eb9cb15bcaa321a0d01097c770
zzennin <53431568+chaelin0722@users.noreply.github.com>
2023-01-17 19:01:57 +0900

    Update deque.py

--- mrcnn/deque.py
+++ mrcnn/deque.py
@@ -22,6 +22,12 @@ class ImageQue:
 
     def qsize(self):
         return len(self.que)
+        
+    def qtolist(self):
+        return list(self.que)
+        
+    def remove(self, rm):
+        return self.que.remove(rm)
 
 
 ######que
@@ -36,6 +42,7 @@ out_que_3 = ImageQue()
 
 in_que_4 = ImageQue()
 out_que_4 = ImageQue()
+
 
 
 class StackingQue(Process):
@@ -61,7 +68,6 @@ class Target():
         event_handler = Handler()
         self.observer.schedule(event_handler, self.watchDir, recursive=True)
         self.observer.start()
-        
         
         event_handler2 = Handler2()
         self.observer2.schedule(event_handler2, self.watchDir2, recursive=True)
@@ -91,6 +97,17 @@ class Handler(FileSystemEventHandler):
             StackingQue.run(in_que_1, cam1_image_dir)
             StackingQue.run(in_que_2, cam1_image_dir)
             
+            # print('out_que_1 size: ', out_que_1.qsize())
+            # print('out_que_2 size: ', out_que_2.qsize())
+            '''
+            for q1 in range(out_que_1.qsize()):
+                output1 = out_que_1.pop()
+                print('queue output1: ', output1)
+            
+            for q2 in range(out_que_2.qsize()):
+                output2 = out_que_2.pop()
+                print('queue output2: ', output2)
+            '''
 
 
 class Handler2(FileSystemEventHandler):
@@ -99,9 +116,17 @@ class Handler2(FileSystemEventHandler):
         # print(event)
         if event.src_path.endswith('.bmp') and event.is_directory is False:
             Handler2.i += 1
+            print('Handler2.i: ', Handler2.i)
             cam2_image_dir = event.src_path
             if Handler2.i > 5:    
                 StackingQue.run(in_que_3, cam2_image_dir)
+
+            # print('out_que_3 size: ', out_que_3.qsize())
+            '''
+            for q3 in range(out_que_3.qsize()):
+                output3 = out_que_3.pop()
+                print('queue output3: ', output3)
+            '''
 
 
 class Handler3(FileSystemEventHandler):
@@ -110,11 +135,19 @@ class Handler3(FileSystemEventHandler):
         # print(event)
         if event.src_path.endswith('.bmp') and event.is_directory is False:
             Handler3.i += 1
+            print('Handler3.i: ', Handler3.i)
+            
             cam3_image_dir = event.src_path
             if Handler3.i > 10:    
                 StackingQue.run(in_que_4, cam3_image_dir)
-
-
+            
+            
+            # print('out_que_4 size: ', out_que_4.qsize())
+            '''
+            for q4 in range(out_que_4.qsize()):
+                output = out_que_4.pop()
+                print('queue output4: ', output4)
+            '''
 
 
 
